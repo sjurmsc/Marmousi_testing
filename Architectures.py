d6a291709d6936865f9dc35e2a21144a74fb4923
Sjur_NGI <=>
2022-12-12 12:23:36 +0100

    typo fix

--- Architectures.py
+++ Architectures.py
@@ -617,7 +617,7 @@ def unsupervised_Marmousi(train_data, config, **kwargs):
     # reg = c_func(1, kernel_size, padding=padding, activation='linear', name='regression_output')(reg)
 
     reg = Flatten()(x)
-    reg = Dense(X.shape[1], activation='linear')
+    reg = Dense(X.shape[1], activation='linear')(reg)
 
     output_layer = [reg] # Regression, reconstruction
 
