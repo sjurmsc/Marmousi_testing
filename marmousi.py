f7a0e48b23df9e8dff697b0bfa59ae868f2ba143
Sjur_NGI <=>
2023-01-09 13:36:37 +0100

    Included prediction image plotting

--- marmousi.py
+++ marmousi.py
@@ -64,7 +64,7 @@ config['nb_reg_stacks']         = 5
 config['nb_rec_stacks']         = 3  
 
 config['batch_size']            = 20
-config['epochs']                = 8
+config['epochs']                = 50
 
 split = len(seis)//2
 train = [seis[:split], ai[:split]]
@@ -75,8 +75,8 @@ model, History = compiled_TCN(train, config=config)
 pred = model.predict(test)
 #pred = pred.reshape(pred.shape[:-1])
 np.save('Data_dumps/pred', pred)
-#plt.imshow(pred[0].T, cmap='Spectral')
-#plt.show()
+plt.imshow(pred[0].T, cmap='Spectral')
+plt.show()
 
 
 
