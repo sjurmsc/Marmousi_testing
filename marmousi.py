2df6ee70baecc83131f26969d97c3d7ae3711e01
Sjur_NGI <=>
2023-01-09 16:22:25 +0100

    rm dims for imshow

--- marmousi.py
+++ marmousi.py
@@ -37,8 +37,6 @@ ai = density*velocity
 # np.save('Data_dumps/SEAM_seismic.npy', seis)
 seis = np.load('Data_dumps/SEAM_seismic.npy')
 
-print(seis.shape, ai.shape)
-
 config = dict()
 config['nb_filters']            = [3, 3, 5, 5, 5, 5, 5]
 config['kernel_size']           = 7 # Height, width
@@ -75,7 +73,7 @@ pred = np.array(pred)
 pred = pred.reshape(pred.shape[:-1])
 # np.save('Data_dumps/pred', pred)
 fig, (upper_ax, lower_ax) = plt.subplots(2)
-upper_ax.imshow(pred[0].T, cmap='Spectral')
+upper_ax.imshow(pred.T, cmap='Spectral')
 lower_ax.imshow(ai[split:].T, cmap='Spectral')
 plt.show()
 
