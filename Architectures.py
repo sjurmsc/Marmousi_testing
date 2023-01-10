c4a76244dd5ed38a536a27d9e0b6babbffd55e57
Sjur_NGI <=>
2023-01-10 10:27:05 +0100

    Added capability for 2D discriminators

--- Architectures.py
+++ Architectures.py
@@ -540,8 +540,14 @@ def compiled_TCN(training_data, config, **kwargs):
     if use_adversaries:
         seis_gen_model = Model(inputs=input_layer, outputs=rec)
         ai_gen_model   = Model(inputs=input_layer, outputs=reg)
-        seis_disc_model = discriminator(output_layer[1].shape[1:], 3, name='seismic_discriminator')
-        ai_disc_model = discriminator(output_layer[0].shape[1:], 3, name='ai_discriminator')
+        seis_disc_model = discriminator(output_layer[1].shape[1:], 
+                                        depth=3,
+                                        convolution_func=convolution_func,
+                                        name='seismic_discriminator')
+        ai_disc_model   = discriminator(output_layer[0].shape[1:], 
+                                        depth=3, 
+                                        convolution_func=convolution_func,
+                                        name='ai_discriminator')
 
 
         model = multi_task_GAN([ai_disc_model, seis_disc_model],
